1db615ab5f5f07772650cf528c5b0efb1976f843
Ronanu <felixrodlsberger@gmail.com>
2022-03-23 17:45:59 +0100

    fehlende tage mit 0 definiert. Schaltjahr ignoriert

--- Klassen.py
+++ Klassen.py
@@ -1,6 +1,6 @@
 import csv
 import re
-
+from datetime import datetime
 
 MonthDict = {
     1: ["January", 31],
@@ -60,7 +60,7 @@ class InPackage:
 
     def read_and_split(self):
 
-        def in_to_dict(date, data, time_on):
+        def in_to_dict(data, date, time_on):
             year, month, day = date
             if year not in data.keys():
                 data[year] = dict()
@@ -75,14 +75,28 @@ class InPackage:
             data[year][month][day]['t'] += time_on
             return data
 
+        def init_data_dict(start_date):
+            curr_date = datetime.today().strftime('%Y-%m-%d')
+            curr_date = [int(d) for d in curr_date.split('-')]
+            cnt = DayCounter(y=start_date[0], m=start_date[1], d=start_date[2])
+            date = start_date
+            data = {}
+            for i in range(3000):
+                data = in_to_dict(data, date, 0.)
+                date = cnt.get_date_increment()
+                if all(d == c for d, c in zip(date, curr_date)):
+                    break
+            if i > 2900:
+                raise IOError('Enddate not found')
+            return data
+
         with open('feeds.csv', newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            data = {}
-            date_counter = None
+            data = None
             for _row in spamreader:
                 try:
                     row = re.split(self.splitter, _row[0])
-                    year = row[0]
+                    year = int(row[0])
                     month = int(row[1])
                     day = int(row[2])
                     time = ':'.join(row[3:6])[:-3]
@@ -90,23 +104,9 @@ class InPackage:
                     time_on = float(row[8])
                     date = [year, month, day]
 
-                    if date_counter is None:
-                        date_counter = DayCounter(y=int(year), m=month, d=day)
-                        next_date = [year, month, day]
-                    else:
-                        next_date = date_counter.get_date_increment()
-
-                    while True:
-                        if all(date[i] == date[i] for i in range(3)):
-                            break
-                        elif all(date[i] == next_date[i] for i in range(3)):
-                            break
-                        else:
-                            next_date = date_counter.get_date_increment()
-                            data = in_to_dict(next_date, data, time_on)
-                            print(next_date)
-
-                    data = in_to_dict(date, data, time_on)
+                    if data is None:
+                        data = init_data_dict(date)
+                    data = in_to_dict(data, date, time_on)
 
                 except:
                     pass
@@ -116,7 +116,9 @@ class InPackage:
 if __name__ == '__main__':
     y, m, d = 2018, 12, 30
     inpt = DayCounter(y=y, m=m, d=d)
-
     diff = inpt.get_date_diff([2019, 4, 6])
-    print(diff)
+
+    inpt = InPackage()
+    data = inpt.read_and_split()
+    print(data)
 
